d7d8edb905609d74aa6901155dbb2bf2aa3706ea
tangchengqin <tangchengqin@52tt.com>
2026-03-20 14:40:36 +0800

    feat:构建流程

--- build.py
+++ build.py
@@ -1,6 +1,5 @@
-from utils.box import Box
-import os
 import shutil
+import os
 
 class Builder:
 
@@ -9,4 +8,23 @@ class Builder:
         self.name = name
 
     def build(self):
-        pass
+        targetPath = os.path.join(self.path, self.name)
+        if os.path.exists(targetPath):
+            raise Exception(f"{targetPath} already exists")
+        os.mkdir(targetPath)
+        # 复制核心文件和导表模板
+        shutil.copytree("./core", os.path.join(targetPath, "core"))
+        os.mkdir(os.path.join(targetPath, "data"))
+        os.mkdir(os.path.join(targetPath, "data", "excel"))
+        os.mkdir(os.path.join(targetPath, "data", "log"))
+        os.mkdir(os.path.join(targetPath, "data", "middle"))
+        os.mkdir(os.path.join(targetPath, "clazz"))
+        shutil.copy("./template/__init__.py", os.path.join(targetPath, "clazz", "__init__.py"))
+        shutil.copy("./template/writer.py", os.path.join(targetPath, "clazz", "writer.py"))
+        shutil.copy("./template/parser.py", os.path.join(targetPath, "parser.py"))
+        shutil.copytree("./template/config", os.path.join(targetPath, "config"))
+
+
+if __name__ == "__main__":
+    builder = Builder("./", "test")
+    builder.build()
