8009c292af9b100e77f85d78948935f35b796f47
tangchengqin <tangchengqin@52tt.com>
2026-03-20 14:32:37 +0800

    feat:代码优化

--- template/parser.py
+++ template/parser.py
@@ -20,29 +20,29 @@ class ParserTemplate(Parser):
             logfile(LogLevel.ERROR, "no clazz path")
             return
         writerList = os.listdir(clazzPath)
-        for fileName in writerList:
-            if fileName == "__init__.py" or fileName[-3:] != ".py":
+        for filename in writerList:
+            if filename == "__init__.py" or filename[-3:] != ".py":
                 continue            
-            target = import_module(f"project.clazz.{fileName[:-3]}")
+            target = import_module(f"project.clazz.{filename[:-3]}")
             if not target:
                 logfile(LogLevel.ERROR, "error writer file path")
                 continue
             if not hasattr(target, "Writer"):
-                logfile(LogLevel.ERROR, f"file {fileName} no writer")
+                logfile(LogLevel.ERROR, f"file {filename} no writer")
                 continue
             Writer = getattr(target, "Writer")
             Writer().write()
 
     def tidy(self):
         path = Box.get(ConfigManager).get("MiddleFile")
-        for fileName in os.listdir(path):
-            if fileName[-5:] != ".json":
+        for filename in os.listdir(path):
+            if filename[-5:] != ".json":
                 continue
-            with open(path + "\\" + fileName, "r", encoding="utf-8") as file1:
+            with open(os.path.join(path, filename), "r", encoding="utf-8") as file1:
                 jsonObj = json.loads(file1.read())
                 jsonObj = sorted(jsonObj.items(), key = lambda x:int(x[0]) if isinstance(x[0], int) else x[0])
                 jsonObj = dict(jsonObj)
-                with open(path + "\\" + fileName, "w", encoding="utf-8") as file2:
+                with open(path + "\\" + filename, "w", encoding="utf-8") as file2:
                     file2.write(json.dumps(jsonObj, indent=4, ensure_ascii=False))
 
     def clearExistFiles(self):
